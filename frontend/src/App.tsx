8fa02aecc97cb4ef0087e169bccce51b1c8f2707
0isoft <octavian.gurlui@gmail.com>
2025-09-21 12:25:29 +0200

    changed icon including favicon

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,32 @@
 // src/App.tsx
 import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+
 
 import Landing from "./pages/Landing";
 import Contact from "./pages/Contact";
 import Menu from "./pages/Menu";
 import Gallery from "./pages/Gallery";
 
-// ⬇️ import directly (no lazy)
 import AdminLogin from "./pages/admin/Login";
 import AdminDashboard from "./pages/admin/Dashboard";
 import RequireAuth from "./admin/RequireAuth";
 import { I18nProvider } from "@/i18n";
 
+import Logo from "@/assets/essentials/orangerose_logo-removebg-preview.png";
+
+
 export default function App() {
+  useEffect(() => {
+    const link: HTMLLinkElement =
+      document.querySelector("link[rel*='icon']") || document.createElement("link");
+
+    link.type = "image/png";
+    link.rel = "icon";
+    link.href = Logo; // this is the imported image, Vite will bundle it
+
+    document.head.appendChild(link);
+  }, []);
   return (
     <div className="min-h-screen w-full bg-[#f3ecdc] text-[#0b0b0b]">
       <I18nProvider>
